da44012bf25afc00f53de397581aa56f07f72923
yashgiri-91 <giriyash805@gmail.com>
2024-06-12 10:22:04 +0530

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,116 @@
+score=0
+name=str(input("enter your name\n "))
+play = input("do you want to start quiz\n ")
+if play != ("yes"):
+    quit ()
+print( "welcome to the quiz \n")
+
+print("1 how many distics in uttarpardesh ")
+print("1- 89 ")
+print("2- 73 ")
+print("3- 75 ") 
+print("4- 78 ")
+ans=int(input("enter your answer \n"))
+ans==3
+
+if ans== 3:
+    print( "great your answer is correct\n" )
+    score+=2
+else:
+    print("opps! your answer is wrong\n")
+
+    
+
+
+
+
+print("2 toughest exam in india ")
+print(" 1- jee  ")
+print(" 2- upsc ")
+print(" 3- neet ") 
+print(" 4- llb ")
+ans=int(input("enter your answer \n"))
+ans==2
+if ans== 2:
+    print( "great your answer is correct\n" )
+    score+=2
+else:
+    print("opps! your answer is wrong\n")
+
+
+
+
+
+print("3 which movie win oscar award currently ")
+print(" 1- 33pushpa ")
+print(" 2- rrr ")
+print(" 3- sikka") 
+print(" 4- bahubali 2 ")
+ans=int(input("enter your answer \n"))
+ans==2
+if ans== 2:
+    print( "great your answer is correct\n" )
+    score+=2
+else:
+    print("opps! your answer is wrong\n")
+
+
+
+
+print("4 distance between sun and earth ")
+print(" 1- 111 million miles ")
+print(" 2- 73.76 million miles ")
+print(" 3- 129 million miles") 
+print(" 4- 150.12 million miles ")
+ans=int(input("enter your answer \n"))
+ans==4
+if ans== 4:
+    print( "great your answer is correct\n" )
+    score+=2
+else:
+    print("opps! your answer is wrong\n")
+
+
+
+
+print("5 what is the heigh of burj khalifa ")
+print(" 1- 894 meters ")
+print(" 2- 733 meters ")
+print(" 3- 828 meters ") 
+print(" 4- 789 meters ")
+ans=int(input("enter your answer \n"))
+ans==3
+if ans==3:
+    print( "great your answer is correct\n" )
+    score+=2
+else:
+    print("opps! your answer is wrong\n")
+
+
+print("great your score is",score,)
+if score== 10:
+    print("CONGRATLATIONS master",name," YOU GOT FULL MARKS ")
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+          
